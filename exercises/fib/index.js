b1d2484591ebdb4bdcd0ccb6b5db6ad01c383f43
Parvez Nawaz <parvez.nawaz@gmail.com>
2019-05-27 07:15:45 -0400

    fib matrix and queue

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,13 +1,64 @@
-// --- Directions
-// Print out the n-th entry in the fibonacci series.
-// The fibonacci series is an ordering of numbers where
-// each number is the sum of the preceeding two.
-// For example, the sequence
-//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-// forms the first ten entries of the fibonacci series.
-// Example:
-//   fib(4) === 3
+// --- Directions Print out the n-th entry in the fibonacci series. The
+// fibonacci series is an ordering of numbers where each number is the sum of
+// the preceeding two. For example, the sequence  [0, 1, 1, 2, 3, 5, 8, 13, 21,
+// 34] forms the first ten entries of the fibonacci series. Example:   fib(4)
+// === 3
 
-function fib(n) {}
+//iterative space complexity 1
+// function fib(n) {
+//     let i = 2,
+//         n1 = 0,
+//         n2 = 1;
+//     if(n==0) return n1;
+//     else if(n==1) return n2;
+//     while (i <= n) {
+//         let t = n1 + n2;
+//         n1 = n2;
+//         n2 = t;
+//         i++;
+//     }
+//     return n2;
+// }
+
+//iterative
+// function fib(n){
+//     const result=[0,1];
+//     for(let i=2;i<=n;i++){
+//         result.push(result[i-1]+result[i-2]);
+//     }
+//     return result[n];
+// }
+
+// function fib(n) {     
+//      if(n<2) return n;     
+//      return fib(n-1)+fib(n-2); 
+// }
+
+
+//memoization
+let arr={};
+// function fib(n) {  
+//     if(n<2) return n;  
+//     if(arr[n]) return arr[n];
+//     arr[n]=fib(n-1)+fib(n-2);
+//     return arr[n]; 
+// }
+
+//memoization with closure
+function memoize(fn){
+    const cache={};
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+        cache[args]=fn.apply(this,args);
+        return cache[args];
+    }
+}
+function slowfib(n){
+    if(n<2) return n;
+    else return fib(n-1)+fib(n-2);
+}
+const fib=memoize(slowfib);
 
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -1,20 +1,53 @@
-// --- Directions
-// Write a function that accepts an integer N
-// and returns a NxN spiral matrix.
-// --- Examples
-//   matrix(2)
-//     [[1, 2],
-//     [4, 3]]
-//   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
-//  matrix(4)
-//     [[1,   2,  3, 4],
-//     [12, 13, 14, 5],
-//     [11, 16, 15, 6],
-//     [10,  9,  8, 7]]
+// --- Directions Write a function that accepts an integer N and returns a NxN
+// spiral matrix. --- Examples   matrix(2)     [[1, 2],     [4, 3]]   matrix(3)
+//    [[1, 2, 3],     [8, 9, 4],     [7, 6, 5]]  matrix(4)     [[1,   2,  3, 4],
+//     [12, 13, 14, 5],     [11, 16, 15, 6],     [10,  9,  8, 7]]
+function display(m) {
+    m.forEach(row => {
+        let str = '';
+        row.forEach(col => {
+            str += col + ' ';
+        });
+        console.log(str);
+    });
+}
+function matrix(n) {
+    let startCol = 0,
+        endCol = n - 1,
+        startRow = 0,
+        endRow = n - 1,
+        counter = 1;
+    let m = new Array(n);
+    for (let i = 0; i < m.length; i++) {
+        m[i] = new Array(n);
+    }
 
-function matrix(n) {}
+    //display(m);
 
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let col = startCol; col <= endCol; col++) {
+            m[startRow][col] = counter++;
+        }
+        startRow++;
+
+        for (let row = startRow; row <= endRow; row++) {
+            m[row][endCol] = counter++;
+        }
+        endCol--;
+
+        for (let col = endCol; col >= startCol; col--) {
+            m[endRow][col] = counter++;
+        }
+        endRow--;
+
+        for (let row = endRow; row >= startRow; row--) {
+            m[row][startCol] = counter++;
+        }
+        startCol++;
+
+    }
+    //display(m);
+    return m;
+}
+matrix(4);
 module.exports = matrix;
